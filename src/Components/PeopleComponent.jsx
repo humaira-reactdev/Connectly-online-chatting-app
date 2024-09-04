e11bbc6efded61500cf248f98ccba0809b48cd58
Humaira Islam <ihumaira.webfront@gmail.com>
2024-09-04 19:24:34 +0600

    Users displayed

--- src/Components/PeopleComponent.jsx
+++ src/Components/PeopleComponent.jsx
@@ -27,28 +27,33 @@ const PeopleComponent = () => {
       <div className="min-h-screen bg-gray-100 py-8 font-montserrat text-[#363636]">
       <h1 className="text-3xl font-bold text-center mb-[10px] text-[#363636]">Connect with others</h1>
       <h2 className='text-xl font-regular text-center mb-8'>Add new friends and start a conversation</h2>
-      <div className="max-w-3xl mx-auto">
-        {users.map((user) => (
+      {
+        allusers.map((item)=>(
+          <div className="max-w-3xl mx-auto">        
           <div
-            key={user.id}
+            key=''
             className="flex items-center justify-between p-4 bg-white shadow-md rounded-lg mb-4"
           >
             <div className="flex items-center">
               <img
                 className="w-12 h-12 object-cover rounded-full"
-                src={user.profilePicture}
-                alt={`${user.name}'s profile`}
+                src={item.userPhoto}
+                alt=''
               />
-              <span className="ml-4 text-lg font-semibold text-[#363636]">
-                {user.name}
+              <span className="ml-4 text-lg font-semibold text-[#363636]">{item.userName}             
               </span>
             </div>
             <button className="flex justify-center items-center gap-[5px] text-white bg-[#8E3E63] hover:bg-[#91DDCF] hover:text-black ease-linear duration-200 font-medium py-[10px] px-[15px] rounded">
             <IoPersonAdd className='text-[15px]'/>Add
             </button>
           </div>
-        ))}
+        
       </div>
+
+        ))
+        
+      }
+      
     </div>
       
     </>
